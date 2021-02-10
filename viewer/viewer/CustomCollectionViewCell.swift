d67a8a67fd4019a96bd6a6109f445b4bc64c3391
Ryan Stich <55109747+RyanStich@users.noreply.github.com>
2021-02-09 22:55:47 -0800

    Add files via upload

---
+++ viewer/viewer/CustomCollectionViewCell.swift
@@ -0,0 +1,80 @@
+//
+//  CustomCollectionViewCell.swift
+//  viewer
+//
+//  Created by Ryan Stich on 2021-02-09.
+
+// handing this in very rough shape, didnt budget myself enough time this week & i'm not sure
+// what was taking place but for a good couple hours i had struggled with the first video
+// i could NOT under any circumstances get images to appear within my collectionview. the concept
+// seemed simple to follow & i even made sure to  follow the youtube tutorial line for line and my app would just open up a blank screen every single time. i eventually just gave up trying to make it work and immediately had success with the following video. not really sure if it was a weird xcode issue on my end or what, because im using the same exact array of images. very strange.
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    static let identifer = "CustomCollectionViewCell"
+    
+    private let myImageVIew: UIImageView = {
+        let imageView = UIImageView()
+//        imageView.image = UIImage(systemName: "house")
+        
+        let images = [
+        
+            UIImage(named:"i1"),
+            UIImage(named:"i2"),
+            UIImage(named:"i3"),
+            UIImage(named:"i4"),
+            UIImage(named:"i5"),
+            UIImage(named:"i6"),
+            UIImage(named:"i7"),
+            UIImage(named:"i8"),
+            UIImage(named:"i9"),
+            UIImage(named:"i10"),
+        ].compactMap({$0})
+        
+        imageView.image = images.randomElement()
+        
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let myLabel: UILabel  = {
+        let label = UILabel()
+        label.text = "hello"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .systemTeal
+        contentView.addSubview(myLabel)
+        contentView.addSubview(myImageVIew)
+        contentView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        myLabel.frame = CGRect(x: 5, y: 0, width: contentView.frame.size.width-10, height: 20)
+        
+        myImageVIew.frame = CGRect(x: 5, y: contentView.frame.size.height-80, width: contentView.frame.size.width-10, height: contentView.frame.size.width-10)
+    }
+    
+    public func configure(label:String) {
+        myLabel.text = label
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        myLabel.text = nil
+    }
+    
+    
+}
---
+++ viewer/viewer/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  viewer
+//
+//  Created by Ryan Stich on 2021-02-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    private var collectionView: UICollectionView?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: view.frame.size.width/3, height: view.frame.size.width/3)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        layout.itemSize = CGSize(width: (view.frame.size.width/3)-4, height: (view.frame.size.width/3)-4)
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifer)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifer, for: indexPath) as! CustomCollectionViewCell
+        cell.configure(label: "Image #\(indexPath.row) ")
+        return cell
+    }
+
+}
+
